a7d01f9e7b688b627c5a5bdc4e32c455989da9f7
Bryanskiy <ivakin.kir@gmail.com>
2023-09-25 16:17:22 +0300

    [ilist] add test + fix append

--- include/ilist.hh
+++ include/ilist.hh
@@ -3,6 +3,11 @@
 #include <memory>
 
 namespace jade {
+
+template<typename T>
+class IList;
+
+
 // intrusive list:
 //
 //  +------+  next   +------+
@@ -17,39 +22,37 @@ namespace jade {
 template<typename T>
 class IListNode {
 public:
-    using Self = IListNode<T>;
+    T* next() { return static_cast<T*>(m_next.get()); }
+    T* prev() { return static_cast<T*>(m_prev); }
 protected:
+    friend class IList<T>;
 
-    std::unique_ptr<Self> m_next{nullptr};
-    Self* m_prev{nullptr};
+    std::unique_ptr<T> m_next{nullptr};
+    T* m_prev{nullptr};
 };
 
 template<typename T>
 class IList {
 public:
-    using Node = typename IListNode<T>::Self;
-
     // requires default T ctor
     T* append() {
         auto elem = std::make_unique<T>();
         if (m_start == nullptr) {
-            m_start = std::move(elem);
             m_last = elem.get();
-            return static_cast<T*>(m_last);
+            m_start = std::move(elem);
+            return m_last;
         }
 
         elem->m_prev = m_last;
         m_last->m_next = std::move(elem);
         auto tmp = m_last->m_next.get();
         m_last = tmp;
-        return static_cast<T*>(m_last);
+        return m_last;
     }
 
 private:
-    friend class IListNode<T>;
-
-    std::unique_ptr<Node> m_start{nullptr};
-    Node* m_last{nullptr};
+    std::unique_ptr<T> m_start{nullptr};
+    T* m_last{nullptr};
 };
 
 } // namespace jade
--- tests/unit/graph_builder.cc
+++ tests/unit/graph_builder.cc
@@ -20,4 +20,20 @@ TEST(GraphBuilder, Check) {
 //      +-----+
 TEST(GraphBuilder, Fib) {
     auto function = jade::Function{};
+
+    auto bb0 = function.append();
+    auto bb1 = function.append();
+    auto bb2 = function.append();
+    auto bb3 = function.append();
+
+    // check intrusive bb list
+    ASSERT_EQ(bb0->prev(), nullptr);
+    ASSERT_EQ(bb0->next(), bb1);
+    ASSERT_EQ(bb1->prev(), bb0);
+    ASSERT_EQ(bb1->next(), bb2);
+    ASSERT_EQ(bb2->prev(), bb1);
+    ASSERT_EQ(bb2->next(), bb3);
+    ASSERT_EQ(bb3->prev(), bb2);
+    ASSERT_EQ(bb3->next(), nullptr);
+    ASSERT_NE(bb0->next(), bb3->prev());
 }
