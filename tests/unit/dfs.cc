8d1e5b6cbd869cef4c4f5bc9aa51c10a81746765
Bryanskiy <ivakin.kir@gmail.com>
2023-10-28 14:32:14 +0300

    [tests] more tests

--- tests/unit/dfs.cc
+++ tests/unit/dfs.cc
@@ -54,3 +54,54 @@ TEST(Dfs, example2G) {
     ASSERT_EQ(*(++dfs), bbs[6]);
     ASSERT_EQ((++dfs), DFSIterator<BasicBlocksGraph>::end(graph));
 }
+
+TEST(Dfs, example3) {
+    auto function = example3();
+    auto graph = function.getBasicBlocks();
+
+    auto range = graph.nodes();
+
+    std::vector<BasicBlock*> bbs;
+    for(auto it = range.begin(); it != range.end(); ++it) {
+        bbs.push_back(&*it);
+    }
+
+    auto dfs = DFSIterator<BasicBlocksGraph>::begin(graph);
+
+    ASSERT_EQ(*dfs, bbs[0]);
+    ASSERT_EQ(*(++dfs), bbs[1]);
+    ASSERT_EQ(*(++dfs), bbs[2]);
+    ASSERT_EQ(*(++dfs), bbs[3]);
+    ASSERT_EQ(*(++dfs), bbs[4]);
+    ASSERT_EQ(*(++dfs), bbs[5]);
+    ASSERT_EQ(*(++dfs), bbs[6]);
+    ASSERT_EQ(*(++dfs), bbs[8]);
+    ASSERT_EQ(*(++dfs), bbs[7]);
+    ASSERT_EQ(*(++dfs), bbs[9]);
+    ASSERT_EQ(*(++dfs), bbs[10]);
+    ASSERT_EQ((++dfs), DFSIterator<BasicBlocksGraph>::end(graph));
+}
+
+TEST(Dfs, example4) {
+    auto function = example4();
+    auto graph = function.getBasicBlocks();
+
+    auto range = graph.nodes();
+
+    std::vector<BasicBlock*> bbs;
+    for(auto it = range.begin(); it != range.end(); ++it) {
+        bbs.push_back(&*it);
+    }
+
+    auto dfs = DFSIterator<BasicBlocksGraph>::begin(graph);
+    ASSERT_EQ(*dfs, bbs[0]);
+    ASSERT_EQ(*(++dfs), bbs[1]);
+    ASSERT_EQ(*(++dfs), bbs[2]);
+    ASSERT_EQ(*(++dfs), bbs[3]);
+    ASSERT_EQ(*(++dfs), bbs[4]);
+    ASSERT_EQ(*(++dfs), bbs[5]);
+    ASSERT_EQ(*(++dfs), bbs[8]);
+    ASSERT_EQ(*(++dfs), bbs[7]);
+    ASSERT_EQ(*(++dfs), bbs[6]);
+    ASSERT_EQ((++dfs), DFSIterator<BasicBlocksGraph>::end(graph));
+}
--- tests/unit/graphs.cc
+++ tests/unit/graphs.cc
@@ -1,5 +1,6 @@
 #include "graphs.hh"
 #include "IR.hh"
+#include "function.hh"
 #include <array>
 
 using namespace jade;
@@ -41,6 +42,59 @@ Function example2() {
     bbs[4]->addSuccessor(bbs[6]);
 
     bbs[6]->addSuccessor(bbs[5]);
+
+    return function;
+}
+
+Function example3() {
+    auto function = Function{};
+
+    std::array<BasicBlock*, 11> bbs;
+    for(std::size_t i = 0; i < bbs.size(); ++i) {
+        bbs[i] = function.create<BasicBlock>();
+    }
+
+    bbs[0]->addSuccessor(bbs[1]);
+    bbs[1]->addSuccessor(bbs[2]);
+    bbs[2]->addSuccessor(bbs[3]);
+    bbs[3]->addSuccessor(bbs[4]);
+    bbs[3]->addSuccessor(bbs[5]);
+    bbs[4]->addSuccessor(bbs[5]);
+    bbs[5]->addSuccessor(bbs[6]);
+    bbs[6]->addSuccessor(bbs[8]);
+    bbs[8]->addSuccessor(bbs[7]);
+    bbs[6]->addSuccessor(bbs[9]);
+    bbs[5]->addSuccessor(bbs[4]);
+    bbs[9]->addSuccessor(bbs[1]);
+    bbs[1]->addSuccessor(bbs[10]);
+    bbs[10]->addSuccessor(bbs[2]);
+    bbs[3]->addSuccessor(bbs[2]);
+
+    return function;
+}
+
+Function example4() {
+    auto function = Function{};
+
+    std::array<BasicBlock*, 9> bbs;
+    for(std::size_t i = 0; i < bbs.size(); ++i) {
+        bbs[i] = function.create<BasicBlock>();
+    }
+
+    bbs[0]->addSuccessor(bbs[1]);
+    bbs[1]->addSuccessor(bbs[2]);
+    bbs[2]->addSuccessor(bbs[3]);
+    bbs[3]->addSuccessor(bbs[4]);
+    bbs[3]->addSuccessor(bbs[5]);
+    bbs[4]->addSuccessor(bbs[5]);
+    bbs[4]->addSuccessor(bbs[2]);
+    bbs[1]->addSuccessor(bbs[8]);
+    bbs[8]->addSuccessor(bbs[7]);
+    bbs[7]->addSuccessor(bbs[6]);
+    bbs[6]->addSuccessor(bbs[5]);
+    bbs[6]->addSuccessor(bbs[4]);
+    bbs[8]->addSuccessor(bbs[3]);
+    bbs[7]->addSuccessor(bbs[1]);
 
     return function;
 }
